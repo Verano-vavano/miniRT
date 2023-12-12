d8e5903817553189bdc9395e80674689595f1770
Hugo Dupire <hugod1383@gmail.com>
2023-12-12 14:55:04 +0100

    Gestion fov

--- srcs/ray_casting.c
+++ srcs/ray_casting.c
@@ -6,12 +6,14 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:16:52 by hdupire           #+#    #+#             */
-/*   Updated: 2023/12/11 12:49:17 by hdupire          ###   ########.fr       */
+/*   Updated: 2023/12/12 14:54:25 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 #include "libft.h"
+
+#define _USE_MATH_DEFINES
 
 /*
 static void	apply_ambient(struct s_ambient a, t_color *c)
@@ -25,7 +27,7 @@ static void	apply_light(struct s_light l, t_vec3 hit, t_color *c)
 {
 	double	d;
 
-	d = fmax(vec3_dot(vec3_mult_float(vec3_normalize(l.pos), -1.0f), hit), 0.0f);
+	d = l.lgt_ratio * fmax(vec3_dot(vec3_mult_float(vec3_normalize(l.pos), -1.0f), hit), 0.0f);
 	c->r = (int) (c->r * d);
 	c->g = (int) (c->g * d);
 	c->b = (int) (c->b * d);
@@ -41,16 +43,19 @@ t_color	cast_ray(t_window *win, t_vec2 *coord)
 	t_vec3	direction;
 	t_vec3	origin;
 	t_color	ret;
-	double	a, b, c, discr, x_hit;
+	float	fov;
+	double	a, b, c, discr, x_hit, x_hit1, x_hit2;
 
 	ret.r = 0;
 	ret.g = 0;
 	ret.b = 0;
-	normalized_coord.x = (2.0f * coord->x / win->width - 1.0f) * win->aspect_ratio;
-	normalized_coord.y = 1.0f - 2.0f * coord->y / win->height;
+	fov = win->scene->camera.fov * M_PI / 180;
+	normalized_coord.x = (2.0f * (coord->x + 0.5f) / win->width - 1.0f) * win->aspect_ratio * tan(fov / 2);
+	normalized_coord.y = (1.0f - 2.0f * (coord->y + 0.5f) / win->height) * tan(fov / 2);
 	direction.x = normalized_coord.x;
 	direction.y = normalized_coord.y;
-	direction.z = 1.0f;
+	direction.z = win->scene->camera.dir.z;
+	direction = vec3_normalize(direction);
 	origin = win->scene->camera.vp;
 	a = vec3_dot(direction, direction);
 	b = 2.0f * vec3_dot(origin, direction);
@@ -61,7 +66,14 @@ t_color	cast_ray(t_window *win, t_vec2 *coord)
 		//apply_ambient(win->scene->ambient, &ret);
 		return (ret);
 	}
-	x_hit = fmin((-b + sqrt(discr)) / (2 * a), (-b - sqrt(discr)) / (2 * a));
+	x_hit1 = (-b + sqrt(discr)) / (2 * a);
+	x_hit2 = (-b - sqrt(discr)) / (2 * a);
+	if (x_hit1 >= 0 && (x_hit2 < 0 || x_hit1 < x_hit2))
+		x_hit = x_hit1;
+	else if (x_hit2 >= 0 && (x_hit1 < 0 || x_hit2 < x_hit1))
+		x_hit = x_hit2;
+	else
+		return (ret);
 	t_vec3	hit = vec3_normalize(vec3_add(origin, vec3_mult_float(direction, x_hit)));
 
 	ret.r = 255;

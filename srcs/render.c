b6d4b1bd02b50205e35e8612d2bb68692c639884
Hugo Dupire <hugod1383@gmail.com>
2024-01-12 17:12:47 +0100

    Corriger petit bug qui faisait que le Shadow bias cassait tout. Il faut que je regarde comment l'implémenter, parce qu'avec des formes plus complexes, ça peut faire des petites bêtises

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 21:32:59 by hdupire           #+#    #+#             */
-/*   Updated: 2024/01/12 16:33:50 by hdupire          ###   ########.fr       */
+/*   Updated: 2024/01/12 17:09:42 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,11 @@ static t_color	cast_ray(t_window *window, t_vec2 *coord, float fov)
 		temp.addr = NULL;
 		get_infos(hit, &last_form, &normal, &lf_color);
 		ambient_lighting(scene->ambient, &amb_contr, lf_color);
-		trace(scene, vec3_add(hit, vec3_mult_float(normal, SHADOW_BIAS)), scene->light.inv_dir, &temp);
+		trace(scene, vec3_add(hit, vec3_mult_float(normal, 0.9)), scene->light.inv_dir, &temp);
 		if (temp.addr == NULL)
 			light_pathing(scene, normal, &lgt_contr, lf_color);
+		else
+			printf("%c\n", temp.shape);
 	}
 	ret.r = (fmin(1.f, lgt_contr.r + amb_contr.r)) * 255;
 	ret.g = (fmin(1.f, lgt_contr.g + amb_contr.g)) * 255;
--- srcs/spheres.c
+++ srcs/spheres.c
@@ -6,30 +6,33 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:16:52 by hdupire           #+#    #+#             */
-/*   Updated: 2024/01/10 20:43:58 by hdupire          ###   ########.fr       */
+/*   Updated: 2024/01/12 16:43:53 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 #include "libft.h"
 
+// a = Dir²
+// Dir normalized, hence x² + y² + z² = 1
+// a = 1, so a isn't required
+// This omission only works if Dir is normalized
 static bool	sphere_intersect(struct s_sphere *sp, t_vec3 org, t_vec3 dir, double *hit)
 {
-	double	a, b, c, discr, q, x_hit1, x_hit2;
+	double	b, c, discr, q, x_hit1, x_hit2;
 	t_vec3	sphere_org_rel;
 
 	sphere_org_rel = vec3_sub(org, sp->pos);
-	a = vec3_dot(dir, dir);
 	b = 2.0f * vec3_dot(sphere_org_rel, dir);
 	c = vec3_dot(sphere_org_rel, sphere_org_rel) - pow(sp->diameter / 2, 2);
-	discr = pow(b, 2) - (4 * a * c);
+	discr = pow(b, 2) - (4 * c);
 	if (discr < 0)
 		return (false);
 	if (b >= 0)
 		q = -(b + discr) / 2;
 	else
 		q = -(b - discr) / 2;
-	x_hit1 = q / a;
+	x_hit1 = q;
 	x_hit2 = c / q;
 	if (x_hit1 >= 0 && (x_hit2 < 0 || x_hit1 < x_hit2))
 		*hit = x_hit1;

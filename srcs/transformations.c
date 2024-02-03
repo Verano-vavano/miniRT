831ec76e4e25c7310e9578afa6dc0d899e1c8193
Verano <hugod1383@gmail.com>
2024-02-03 04:05:52 +0100

    translation des sphères

--- srcs/transformations.c
+++ srcs/transformations.c
@@ -6,13 +6,24 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 03:48:12 by hdupire           #+#    #+#             */
-/*   Updated: 2024/02/03 03:49:11 by hdupire          ###   ########.fr       */
+/*   Updated: 2024/02/03 04:04:32 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 
+static void	sphere_transform(t_sphere *sp, t_transform t)
+{
+	if (t.type == 'r')
+		return ;
+	sp->pos.x += t.x;
+	sp->pos.y += t.y;
+	sp->pos.z += t.z;
+}
+
 void	apply_transformation(t_lform select, t_transform transform)
 {
+	if (select.shape == 's')
+		sphere_transform((t_sphere *) select.addr, transform);
 	printf("%c %d %d %d\n", select.shape, transform.x, transform.y, transform.z);
 }
